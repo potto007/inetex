b6ed930fe2d3117bbbbacf7fab73b1415470565f
Paul Otto <paul@ottoops.com>
2016-03-01 23:25:59 -0700

    Basic tests created.

--- lib/inetex.ex
+++ lib/inetex.ex
@@ -1,2 +1,19 @@
 defmodule Inetex do
+
+  def subnet({i1, i2, i3, _i4}) do
+    {i1, i2, i3, 0}
+  end
+
+  def ip_to_str(ipaddress) do
+    :inet.ntoa(ipaddress)
+    |> to_string
+  end
+
+  def str_to_ip(ipstr) do
+    ipstr
+    |> to_char_list
+    |> :inet.parse_address
+    |> elem(1)
+  end
+
 end
--- test/inetex_test.exs
+++ test/inetex_test.exs
@@ -1,8 +1,17 @@
 defmodule InetexTest do
   use ExUnit.Case
-  doctest Inetex
+#  doctest Inetex
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "changes tuple-based ipaddress into string" do
+    assert Inetex.ip_to_str({127,0,0,1}) ==  "127.0.0.1"
   end
+
+  test "changes ipaddress string into tuple" do
+    assert Inetex.str_to_ip("127.0.0.1") == {127,0,0,1}
+  end
+
+  test "subnet created from ipaddress" do
+    assert Inetex.subnet({127,0,0,1}) == {127,0,0,0}
+  end
+
 end
